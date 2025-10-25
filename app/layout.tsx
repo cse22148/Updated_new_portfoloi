69a88df2679e6eef4541c1fbd890501668bf962d
Chanchal Teotia <152671330+cse22148@users.noreply.github.com>
2025-10-25 16:37:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'chanchal _ portfolio',
   description: 'Created with v0',
-  generator: 'v0.app',
+ 
 }
 
 export default function RootLayout({
